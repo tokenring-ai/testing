tokenring-ai/codebase

"Fix inconsistent spacing and import formatting"
This commit standardizes indentation, spacing, and import formatting across multiple files to improve code readability and maintainability. No functional changes are included.

index.ts
@@ -1,54 +1,67 @@
-import type {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
-import {z} from "zod";
+import type { AgentTeam, TokenRingPackage } from "@tokenring-ai/agent";
+import { z } from "zod";
 import * as chatCommands from "./chatCommands.ts";
 import CodeBaseService from "./CodeBaseService.ts";
 import FileTreeResource from "./FileTreeResource.ts";
-import packageJSON from "./package.json" with {type: "json"};
+import packageJSON from "./package.json" with { type: "json" };
 import RepoMapResource from "./RepoMapResource.ts";
 import * as tools from "./tools.ts";
 import WholeFileResource from "./WholeFileResource.ts";
 
-export const CodeBaseConfigSchema = z.object({
-  resources: z.record(z.string(), z.any()),
-  default: z.object({
-    resources: z.array(z.string())
-  }).optional()
-}).optional();
+export const CodeBaseConfigSchema = z
+	.object({
+		resources: z.record(z.string(), z.any()),
+		default: z
+			.object({
+				resources: z.array(z.string()),
+			})
+			.optional(),
+	})
+	.optional();
 
 export const packageInfo: TokenRingPackage = {
 	name: packageJSON.name,
 	version: packageJSON.version,
 	description: packageJSON.description,
-  install(agentTeam: AgentTeam) {
-    const config = agentTeam.getConfigSlice('codebase', CodeBaseConfigSchema);
-    if (config) {
-      agentTeam.addTools(packageInfo, tools);
-      agentTeam.addChatCommands(chatCommands);
-      const codebaseService = new CodeBaseService();
-      agentTeam.addServices(codebaseService);
+	install(agentTeam: AgentTeam) {
+		const config = agentTeam.getConfigSlice("codebase", CodeBaseConfigSchema);
+		if (config) {
+			agentTeam.addTools(packageInfo, tools);
+			agentTeam.addChatCommands(chatCommands);
+			const codebaseService = new CodeBaseService();
+			agentTeam.addServices(codebaseService);
 
-      for (const name in config.resources) {
-        const resourceConfig = config.resources[name];
-        switch (resourceConfig.type) {
-          case "fileTree":
-            codebaseService.registerResource(name, new FileTreeResource(resourceConfig));
-            break;
-          case "repoMap":
-            codebaseService.registerResource(name, new RepoMapResource(resourceConfig));
-            break;
-          case "wholeFile":
-            codebaseService.registerResource(name, new WholeFileResource(resourceConfig));
-            break;
-        }
-      }
-      if (config.default?.resources) {
-        codebaseService.enableResources(config.default.resources);
-      }
-    }
-  }
+			for (const name in config.resources) {
+				const resourceConfig = config.resources[name];
+				switch (resourceConfig.type) {
+					case "fileTree":
+						codebaseService.registerResource(
+							name,
+							new FileTreeResource(resourceConfig),
+						);
+						break;
+					case "repoMap":
+						codebaseService.registerResource(
+							name,
+							new RepoMapResource(resourceConfig),
+						);
+						break;
+					case "wholeFile":
+						codebaseService.registerResource(
+							name,
+							new WholeFileResource(resourceConfig),
+						);
+						break;
+				}
+			}
+			if (config.default?.resources) {
+				codebaseService.enableResources(config.default.resources);
+			}
+		}
+	},
 };
 
-export {default as FileTreeResource} from "./FileTreeResource.ts";
-export {default as RepoMapResource} from "./RepoMapResource.ts";
-export {default as WholeFileResource} from "./WholeFileResource.ts";
-export {default as CodeBaseService} from "./CodeBaseService.ts";
+export { default as FileTreeResource } from "./FileTreeResource.ts";
+export { default as RepoMapResource } from "./RepoMapResource.ts";
+export { default as WholeFileResource } from "./WholeFileResource.ts";
+export { default as CodeBaseService } from "./CodeBaseService.ts";

tokenring-ai/testing

index.ts
@@ -1,48 +1,55 @@
-import type {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
-import {z} from "zod";
+import type { AgentTeam, TokenRingPackage } from "@tokenring-ai/agent";
+import { z } from "zod";
 import * as agents from "./agents.ts";
 import * as chatCommands from "./chatCommands.ts";
 import * as hooks from "./hooks.ts";
-import packageJSON from "./package.json" with {type: "json"};
+import packageJSON from "./package.json" with { type: "json" };
 import ShellCommandTestingResource from "./ShellCommandTestingResource.ts";
 import TestingService from "./TestingService.ts";
 
-export const TestingConfigSchema = z.object({
-  resources: z.record(z.string(), z.any()).optional(),
-  default: z.object({
-    resources: z.array(z.string())
-  }).optional()
-}).optional();
+export const TestingConfigSchema = z
+	.object({
+		resources: z.record(z.string(), z.any()).optional(),
+		default: z
+			.object({
+				resources: z.array(z.string()),
+			})
+			.optional(),
+	})
+	.optional();
 
 export const packageInfo: TokenRingPackage = {
 	name: packageJSON.name,
 	version: packageJSON.version,
 	description: packageJSON.description,
-  install(agentTeam: AgentTeam) {
-    const config = agentTeam.getConfigSlice('testing', TestingConfigSchema);
-    if (config) {
-      agentTeam.addChatCommands(chatCommands);
-      agentTeam.addHooks(packageInfo, hooks);
-      agentTeam.addAgentConfigs(agents);
-      const testingService = new TestingService();
-      agentTeam.addServices(testingService);
+	install(agentTeam: AgentTeam) {
+		const config = agentTeam.getConfigSlice("testing", TestingConfigSchema);
+		if (config) {
+			agentTeam.addChatCommands(chatCommands);
+			agentTeam.addHooks(packageInfo, hooks);
+			agentTeam.addAgentConfigs(agents);
+			const testingService = new TestingService();
+			agentTeam.addServices(testingService);
 
-      if (config.resources) {
-        for (const name in config.resources) {
-          const testingConfig = config.resources[name];
-          switch (testingConfig.type) {
-            case "shell-testing":
-              testingService.registerResource(name, new ShellCommandTestingResource(testingConfig));
-              break;
-          }
-        }
-      }
-      if (config.default?.resources) {
-        testingService.enableResources(config.default.resources);
-      }
-    }
-  }
+			if (config.resources) {
+				for (const name in config.resources) {
+					const testingConfig = config.resources[name];
+					switch (testingConfig.type) {
+						case "shell-testing":
+							testingService.registerResource(
+								name,
+								new ShellCommandTestingResource(testingConfig),
+							);
+							break;
+					}
+				}
+			}
+			if (config.default?.resources) {
+				testingService.enableResources(config.default.resources);
+			}
+		}
+	},
 };
 
-export {default as TestingService} from "./TestingService.ts";
-export {default as ShellCommandTestingResource} from "./ShellCommandTestingResource.ts";
+export { default as TestingService } from "./TestingService.ts";
+export { default as ShellCommandTestingResource } from "./ShellCommandTestingResource.ts";